b7e6192e4739e99463acf0b9b311ce540d954ee0
Mateus Auler <mateus.auler@sga.pucminas.br>
2019-09-06 22:12:43 -0300

    Javadoc and new parameter names in Registro.java

--- CRUD/src/main/java/br/pucminas/crud/Registro.java
+++ CRUD/src/main/java/br/pucminas/crud/Registro.java
@@ -3,9 +3,30 @@ import java.io.IOException;
 
 public interface Registro {
     
+    /**
+     * Retorna o ID do registro
+     * @return ID do registro
+     */
     public int getID();
-    public void setID(int n);
+
+    /**
+     * Grava o ID
+     * @param _id ID
+     */
+    public void setID(int _id);
+
+    /**
+     * Converte o registro em um array de bytes, com seus dados
+     * @return O array de bytes
+     * @throws IOException
+     */
     public byte[] toByteArray() throws IOException;
-    public void fromByteArray( byte[] ba ) throws IOException;
+
+    /**
+     * Preenche os campos a partir de um array de bytes
+     * @param _byteData Array com os dados
+     * @throws IOException
+     */
+    public void fromByteArray( byte[] _byteData ) throws IOException;
     
 }
