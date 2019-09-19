04f03455e7e76305772ac6bebbf686f1b34bb1fb
henrique-temponi <tanzenheavely@gmail.com>
2019-09-19 01:07:35 -0300

    Começa a implementação da API de filmes

---
+++ CRUD/src/main/java/br/pucminas/crud/ImdbApi.java
@@ -0,0 +1,10 @@
+package br.pucminas.crud;
+
+public class ImdbApi {
+	//fazer a api :)
+	/*
+	 * https://www.baeldung.com/java-http-request
+	 * https://dzone.com/articles/how-to-implement-get-and-post-request-through-simp
+	 * http://www.omdbapi.com/
+	 */
+}
